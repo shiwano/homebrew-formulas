d933db594ddd84be1b1ef7def15f21e788ff6427
Shogo Iwano <shiwano@gmail.com>
2016-01-01 16:02:41 +0900

    Bump robo version to 0.4.0

--- robo.rb
+++ robo.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Robo < Formula
   homepage 'https://github.com/tj/robo'
-  url 'https://github.com/tj/robo/releases/download/v0.3.0/robo_darwin_amd64'
-  sha256 '73bea1b58105e5b5575d9769ec60edf6d6e1a1ac42fa37a2ee028fd1bd3f9fdc'
+  url 'https://github.com/tj/robo/releases/download/v0.4.0/robo_darwin_amd64'
+  sha256 '380dd262ba585cd0f54e2e7c6aa91dac0cf7b32545544de6eae003a7dcdf0928'
 
-  version 'v0.3.0'
+  version 'v0.4.0'
 
   def install
     mv 'robo_darwin_amd64', 'robo'
