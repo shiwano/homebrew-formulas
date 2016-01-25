cb292e97397f75d8dc730645c022bbbd390b1243
Shogo Iwano <shiwano@gmail.com>
2016-01-26 01:33:48 +0900

    Bump master version to 0.3.0

--- master.rb
+++ master.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Master < Formula
   homepage 'https://github.com/shiwano/master'
-  url "https://github.com/shiwano/master/releases/download/v0.2.0/master_darwin_amd64"
-  sha256 'b46e7ecb0862f1cbd9a74bc923755f0c8eeff645b0656fa1023c42edabe5f5c3'
+  url "https://github.com/shiwano/master/releases/download/v0.3.0/master_darwin_amd64"
+  sha256 '4eb99fdff7d87fbb9ec97d535f8473bc014fbe78a26019fa4eec41a3a5dcf993'
 
-  version 'v0.2.0'
+  version 'v0.3.0'
 
   def install
     mv 'master_darwin_amd64', 'master'
