7c5572fb5732b7ce2c7b5fa325abd504619b2446
Shogo Iwano <shiwano@gmail.com>
2017-06-18 22:46:24 +0900

    Update misakura.rb

--- misakura.rb
+++ misakura.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Misakura < Formula
   homepage 'https://github.com/shiwano/misakura'
   url "https://github.com/shiwano/misakura/archive/v0.1.1.tar.gz"
-  sha1 "279035a33ca149815fab153222470c9621d7a7ee"
+  sha256 "d12909687d07f03c0583933eae003f830250b4c62161434f678acb825afc41ce"
 
   version 'v0.1.1'
 
