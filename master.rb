39b0ced5182178b6c1e747c3b4432d84ffeefbe2
Shogo Iwano <shiwano@gmail.com>
2017-06-26 03:36:42 +0900

    Update master.rb

--- master.rb
+++ master.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Master < Formula
   homepage 'https://github.com/shiwano/master'
-  url "https://github.com/shiwano/master/releases/download/v0.4.0/master_darwin_amd64"
-  sha256 '31051fcd0a13d05afdd3f599398685f7dc223f178ab44344961c108094a2255f'
+  url "https://github.com/shiwano/master/releases/download/v0.5.0/master_darwin_amd64"
+  sha256 'ba4dc7d3f16761f14849ccb6b96ca2e9465ac98aa2ff0242589bfe2e0b3c25e1'
 
-  version 'v0.4.0'
+  version 'v0.5.0'
 
   def install
     mv 'master_darwin_amd64', 'master'
